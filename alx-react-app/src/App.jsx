ba686f60776b2d9bd6bac09bde0d946950acf60f
Uduakobongg <uduakobong750@gmail.com>
2025-07-10 11:09:29 +0100

    Add commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import WelcomeMessage  from './components/WelcomeMessage'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'  
 
 function App() {
  return (
     <>
+    <Header />
+    <MainContent />
     <WelcomeMessage />
+    <Footer />
     
     </>
   )
