97000953c8077e346b166e749389ccec70b38001
Uduakobongg <uduakobong750@gmail.com>
2025-07-10 11:18:14 +0100

    Add all commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,14 +3,20 @@ import WelcomeMessage  from './components/WelcomeMessage'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'  
-
+import UserProfile from './components/UserProfile'
 function App() {
  return (
     <>
     <Header />
     <MainContent />
     <WelcomeMessage />
+    <UserProfile 
+    name="Ginna"
+    age="31"
+    bio="Gina Godwin is a front-end developer and UX/UI designer passionate about creating intuitive, user-friendly digital experiences. "
+    />
     <Footer />
+
     
     </>
   )
