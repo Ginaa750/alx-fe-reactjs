4bc1d1f9b94f23fb9fa99c4ab1bc4556f3483616
Uduakobongg <uduakobong750@gmail.com>
2025-07-24 14:11:50 +0100

    Add recipe store using Zustand

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,9 +2,13 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+
   addRecipe: (recipe) => set((state) => ({
     recipes: [...state.recipes, recipe],
   })),
+
+  // ✅ Add this function
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
 }));
 
 export default useRecipeStore;
