ad3bb2cf4ab93bfd9229ec2a1996b8171cc9c13e
Uduakobongg <uduakobong750@gmail.com>
2025-08-10 14:47:09 +0100

    Add update

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import UserProfile from './components/UserProfile';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,12 +18,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
 
       {/* Tailwind test heading */}
       <h1 className="text-3xl font-bold underline text-pink-500">
         Hello Tailwind!
       </h1>
+
+      {/* Show the UserProfile component */}
+      <UserProfile />
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -31,11 +37,12 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
